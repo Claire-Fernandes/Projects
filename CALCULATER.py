b866f8236cc2980f7f4a9260e8341825c75c5db6
Claire-Fernandes <73990796+Claire-Fernandes@users.noreply.github.com>
2020-11-05 14:58:54 +0530

    Add files via upload

---
+++ CALCULATER.py
@@ -0,0 +1,25 @@
+X=int(input("enter 1st number:"))
+Y=int(input("enter 2st number:"))
+op=input("enter the operator:")
+if op=="+":
+    ans=X+Y
+    print('addition of 2 numbers are:', ans)
+elif op=="-":
+    sub=X-Y
+    print('Subtraction of 2 numbers are:', sub)
+elif op=="*":
+    mu=X*Y
+    print('multiplication of 2 numbers are:', mu)
+elif op=="/":
+    div=X/Y
+    print('division of 2 numbers are:', div)
+elif op=="%":
+    MO=X%Y
+    print('remainder of 2 numbers are:',MO)
+"""
+print('addition of 2 numbers are:',ans)
+print('division of 2 numbers are:',div)
+print('remainder of 2 numbers are:',MO)
+print('multiplication of 2 numbers are:',M)
+print('Subtraction of 2 numbers are:',sub)
+"""
